b7db6ad41bed22e037593bb2bc64be24077c5b28
vinod3219 <47097818+vinod3219@users.noreply.github.com>
2019-02-03 16:18:17 +0530

    Update Login.java

--- testEmpDemo/Login.java
+++ testEmpDemo/Login.java
@@ -1,5 +1,8 @@
 package testEmpDemo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -8,29 +11,53 @@ import org.openqa.selenium.WebElement;
 public class Login extends SetUp {
 
 	@Test
-	public void testLogin() throws InterruptedException{
+	public void testLoginFailure() throws InterruptedException{
+		loginSubmit("admin","admin123");
+		WebElement errormsg = driver.findElement(By.tagName("font"));
+	    Assert.assertTrue("Message is expected for display", errormsg.isDisplayed());
+	    Assert.assertEquals("Message is expected for display","Invalid Login",errormsg.getText());
+	    checkForm();
+	}
+	
+	@Test
+	public void testLoginSucess() throws InterruptedException{
+		loginSubmit("admin","admin");
+		
+		
+	}
+
+	private void loginSubmit(String uname,String pwd) throws InterruptedException {
 		driver.get("http://localhost:9080/EmpDemo/");
 		Thread.sleep(3000);
+		WebElement login = checkForm();
+		driver.findElement(By.name("loginName")).sendKeys(uname);
+		driver.findElement(By.name("password")).sendKeys(pwd);
+		login.submit();
+	}
+
+	private WebElement checkForm() throws InterruptedException {
 		WebElement header = driver.findElement(By.tagName("h1"));
-		Assert.assertTrue("Login Page"+ " is expected for display", header.isDisplayed());
+		Assert.assertTrue("Login Page" + " is expected for display", header.isDisplayed());
+		
+		Map<String,String> map = new HashMap<>();
+		map.put("userNameLbl", "User Name:");
+		map.put("passwordLbl", "Password:");
+		assertLabels(map);
+		
 		testVisibleAndEnable("loginName","password");
-		driver.findElement(By.name("loginName")).sendKeys("admin");
-		driver.findElement(By.name("password")).sendKeys("admin123");
+		
 		Thread.sleep(3000);
 		WebElement login = driver.findElement(By.tagName("input"));
 		Assert.assertTrue("login button is expected for display", login.isDisplayed());
 		Assert.assertTrue("login button is enable", login.isEnabled());
-		login.submit();
-		WebElement errormsg = driver.findElement(By.tagName("font"));
-	    Assert.assertTrue("Message is expected for display", errormsg.isDisplayed());
-		System.out.println(errormsg);
+		return login;
 	}
+	
 	private void testVisibleAndEnable(String... names){
 		for(String name: names){
 			WebElement Name = driver.findElement(By.name(name));
 			Assert.assertTrue(name+ " is expected for display", Name.isDisplayed());
 			Assert.assertTrue(name +" to enable", Name.isEnabled());
-	
 		}
 	}
 }
